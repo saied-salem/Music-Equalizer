b19f23fba2001cf7b98dfecb032a6ed717b17e4f
unknown <saied.salem44@gmail.com>
2022-02-16 05:13:38 +0200

    creating sectrogram viewer

---
+++ src/spectogram_viewer.py
@@ -0,0 +1,27 @@
+import matplotlib as matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as navigation_toolbar
+from matplotlib.figure import Figure
+plt.style.use('dark_background')
+matplotlib.use('Qt5Agg')
+
+
+class Spectogram_viewer(FigureCanvas):
+
+    def __init__(self, parent=None, width=0.1, height=0.01, dpi=100):
+        self.fig = Figure()
+        self.axes = self.fig.add_subplot(111)
+        for spine in ['right', 'top', 'left', 'bottom']:
+            self.axes.spines[spine].set_color('gray')
+        # self.axes.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+        super().__init__(self.fig)
+        self.fig.tight_layout()
+
+    def draw_spectogram(self,data):
+        self.axes.specgram(data)
+        self.draw()
+
+    def clear_canvans(self):
+        self.axes.clear()
+        self.draw()
